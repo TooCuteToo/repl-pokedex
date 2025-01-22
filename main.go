2a9c2fb387bc78c6ffc3f42ace561909de7f4726
TooCuteToo <bao.2001181020@gmail.com>
2025-01-22 19:38:35 +0700

    add: main logic for the repl (READ EVAL PRINT LOOP)
    Signed-off-by: TooCuteToo <bao.2001181020@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		line := scanner.Text()
+
+		words := strings.Fields(line)
+		loweredWords := []string{}
+
+		for _, w := range words {
+			loweredWords = append(loweredWords, strings.ToLower(w))
+		}
+
+		fmt.Printf("Your command was: %v\n", loweredWords[0])
+	}
 }
 
 func cleanInput(text string) []string {
